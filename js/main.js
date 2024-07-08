79c195a94ea4e85dbce16d95338eaf028d1bf3cc
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-07-08 13:09:28 +0500

    init

--- js/main.js
+++ js/main.js
@@ -8,14 +8,14 @@ let showAllTodosLength = document.querySelector(".showAllTodosLength");
 let showCompletedTodosLength = document.querySelector(".showCompletedTodosLength");
 let showUncompletedTodosLength = document.querySelector(".showUncompletedTodosLength");
 let showDeletedTodosLength = document.querySelector(".showDeletedTodosLength");
-let wrapperModal = document.querySelector("#wrapperModal");
+let wrapperModal = document.querySelector("#wrapperModal")
 
-let todoArr = loadFromLocalStorage('todoArr') || [];
-let deletedTodoArr = loadFromLocalStorage('deletedTodoArr') || [];
+let todoArr = [];
+let deletedTodoArr = [];
 
 showDeletedTodos.addEventListener("click", () => {
     if (deletedTodoArr.length == 0) {
-        todosCon.innerHTML = '<h1 class="font-bold text-center text-gray-500">Not yet deleted todos</h1>';
+        todosCon.innerHTML = '<h1 class="font-bold text-center text-gray-500">Not yet deleted todos</h1'
     } else {
         renderTodos(deletedTodoArr, todosCon);
     }
@@ -27,16 +27,18 @@ showAllTodos.addEventListener("click", () => {
 
 showCompletedTodos.addEventListener('click', () => {
     if (todoArr.filter(todo => todo.completed).length == 0) {
-        todosCon.innerHTML = '<h1 class="font-bold text-center text-gray-500">Not yet completed todos</h1>';
+        todosCon.innerHTML = '<h1 class="font-bold text-center text-gray-500">Not yet completed todos</h1'
     } else {
         renderTodos(todoArr.filter(todo => todo.completed), todosCon);
+
     }
 });
 
 showUncompletedTodos.addEventListener('click', () => {
     if (todoArr.filter(todo => !todo.completed).length == 0) {
-        todosCon.innerHTML = '<h1 class="font-bold text-center text-gray-500">Not yet uncompleted todos</h1>';
+        todosCon.innerHTML = '<h1 class="font-bold text-center text-gray-500">Not yet uncompleted todos</h1'
     } else {
+
         renderTodos(todoArr.filter(todo => !todo.completed), todosCon);
     }
 });
@@ -51,17 +53,16 @@ form.addEventListener("submit", e => {
         title: todoValue,
         completed: false
     };
-    const existingTodo = todoArr.find((item) => item.title == todoObj.title);
+    const existingTodo = todoArr.find((item) => item.title == todoObj.title)
     if (!existingTodo) {
         todoArr.push(todoObj);
     } else {
-        alert("This Todo is already exist");
+        alert("This Todo is already exist")
     }
     e.target.reset();
-    todosCon.classList.remove("hidden");
+    todosCon.classList.remove("hidden")
     renderTodos(todoArr, todosCon);
-    updateTodosLength();
-    saveToLocalStorage('todoArr', todoArr);
+    updateTodosLenght()
 });
 
 function renderTodos(arr, list) {
@@ -77,78 +78,67 @@ function renderTodos(arr, list) {
             </div>
             <div class="flex items-center gap-3">
                 <i class="fa-regular p-2 rounded-md text-white cursor-pointer duration-300 hover:bg-green-500 font-bold bg-green-500/50 fa-pen-to-square" onclick="editTodo(${todo.id})"></i>
-                <i class="fa-regular p-2 rounded-md text-white cursor-pointer duration-300 hover:bg-red-500 font-bold bg-red-500/50 fa-square-minus" onclick="deleteTodo(${todo.id})"></i>
+                <i class="fa-regular p-2 rounded-md text-white cursor-pointer duration-300 hover:bg-red-500 font-bold bg-red-500/50 fa-square-minus" onclick="deletedTodo(${todo.id})"></i>
             </div>
         `;
         li.querySelector("input").addEventListener("click", () => {
             todo.completed = !todo.completed;
             renderTodos(todoArr, todosCon);
-            updateTodosLength();
-            saveToLocalStorage('todoArr', todoArr);
+            updateTodosLenght()
         });
         list.append(li);
     });
 }
 
-function deleteTodo(id) {
+function deletedTodo(id) {
     const todoToDelete = todoArr.find(todo => todo.id === id);
     if (todoToDelete) {
         deletedTodoArr.push(todoToDelete);
         todoArr = todoArr.filter(todo => todo.id !== id);
         renderTodos(todoArr, todosCon);
-        saveToLocalStorage('todoArr', todoArr);
-        saveToLocalStorage('deletedTodoArr', deletedTodoArr);
+        console.log(deletedTodoArr);
     }
-    updateTodosLength();
+    updateTodosLenght()
 }
 
 function editTodo(id) {
-    wrapperModal.classList.add("!top-0");
-    const findTodo = todoArr.find(item => item.id == id);
+    wrapperModal.classList.add("!top-0")
+    const findTodo = todoArr.find(item => item.id == id)
     wrapperModal.innerHTML = `
-        <div class="absolute top-[50%] left-[50%] w-[600px] h-[400px] bg-pink-400 text-white translate-x-[-50%] translate-y-[-50%]">
+                <div class="absolute top-[50%] left-[50%] w-[600px] h-[400px] bg-pink-400 text-white translate-x-[-50%] translate-y-[-50%]">
             <div class="flex flex-col items-center justify-center p-5">
                 <h2 class="font-bold text-2xl">Edit Todo</h2>
+
                 <div class="flex flex-col gap-3">
                     <input type="text" id="editTodoInput" class="w-full text-black px-3 py-2 rounded-md" placeholder="Edit todo" value="${findTodo.title}">
                     <button onclick={updateTodo(${id})} class="w-full px-3 py-2 mt-3 bg-clifford hover:bg-clifford/60 duration-300 text-white rounded-md">Save</button>
                 </div>
+
             </div>
         </div>
-    `;
+    `
+    console.log(findTodo);
 }
 
-function updateTodo(id) {
-    const findTodo = todoArr.find(item => item.id == id);
-    if (findTodo) {
-        findTodo.title = document.getElementById('editTodoInput').value;
+function updateTodo(id){
+    const findTodo = todoArr.find(item => item.id == id)
+    if(findTodo){
+        findTodo.title = document.getElementById('editTodoInput').value
         renderTodos(todoArr, todosCon);
-        updateTodosLength();
-        saveToLocalStorage('todoArr', todoArr);
-        wrapperModal.classList.remove("!top-0");
+        updateTodosLenght()
+        wrapperModal.classList.remove("!top-0")
     }
 }
 
 wrapperModal.addEventListener('click', (e) => {
-    if (e.target.id == "wrapperModal") {
-        wrapperModal.classList.remove("!top-0");
+    if(e.target.id == "wrapperModal"){
+        wrapperModal.classList.remove("!top-0")
     }
-});
+})
 
-function updateTodosLength() {
+function updateTodosLenght() {
     showAllTodosLength.textContent = todoArr.length;
     showCompletedTodosLength.textContent = todoArr.filter(todo => todo.completed).length;
     showUncompletedTodosLength.textContent = todoArr.filter(todo => !todo.completed).length;
     showDeletedTodosLength.textContent = deletedTodoArr.length;
 }
-
-function saveToLocalStorage(key, data) {
-    localStorage.setItem(key, JSON.stringify(data));
-}
-
-function loadFromLocalStorage(key) {
-    return JSON.parse(localStorage.getItem(key));
-}
-
-renderTodos(todoArr, todosCon);
-updateTodosLength();
